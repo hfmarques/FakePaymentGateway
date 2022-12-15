d5bc2e2165f0995b56048b139c22a08c4ee988cc
Héber Marques <heber.f.marques@gmail.com>
2022-12-15 17:29:19 -0300

    Added Api

---
+++ FakePaymentGateway.Api/Apis/PaymentApis.cs
@@ -0,0 +1,16 @@
+﻿namespace FakePaymentGateway.Api.Apis;
+
+public static class PaymentApis
+{
+    public static RouteGroupBuilder MapPayments(this IEndpointRouteBuilder routes)
+    {
+        var groupBuilder = routes.MapGroup("/payments");
+
+        groupBuilder.WithTags("Payments");
+
+        groupBuilder.MapGet("/info",
+            () => $"{DateTime.Now.ToString()} - {Environment.OSVersion} - {Environment.MachineName}");
+
+        return groupBuilder;
+    }
+}
